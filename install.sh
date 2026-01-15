32ac5b422cb5f67abe649808fee13a109168912f
Mikola 2.0 <gnots.nik@gmail.com>
2026-01-15 18:14:48 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,16 @@
+mkdir ~/soft
+cd ~/soft
+git clone https://github.com/Nick-cpp/wefetch
+cd wefetch/
+gcc wefetch.c -o wefetch -O3 -march=native
+sudo mv wefetch /usr/bin/
+mkdir -p ~/.config/wefetch/logos/
+cp logos/* ~/.config/wefetch/logos/
 
+cd ~/soft
+git clone https://github.com/Nick-cpp/fflash
+cd fflash
+g++ fflash.cpp -o fflash -O3 -march=native
+sudo mv fflash /usr/bin/
+
+cd ~/soft
